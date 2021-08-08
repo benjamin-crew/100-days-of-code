9136fe37c197f651ef906f143f569a0e25d51b56
Benjamin Crew <benncrew94@gmail.com>
2021-08-08 14:33:28 +0100

    Day 11 - Complete

--- 11/blackjack.py
+++ 11/blackjack.py
@@ -2,116 +2,103 @@ from blackjack_art import logo
 import random
 import sys
 
-def sum_of_list(list):
-    list_sum = 0
-    for n in list:
-        list_sum += n
-    
-    return list_sum
-
 
 def start_game():
-    """This function starts the game and asks if you would like to play"""
+    """Main game loop"""
     play = input("Would you like to play? Yes/No: ").lower()
     if play == "yes":
-        initialise_deck()
+        scores_dict['played'] += 1
+        deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]*16
+
+        # Shuffle deck and assign hands
+        random.shuffle(deck)
+
+        player_hand = []
+        dealer_hand = []
+
+        for _ in range(2):
+            draw_card(player_hand, deck)
+            draw_card(dealer_hand, deck)
+
+        play_player(deck, player_hand, dealer_hand)
+
+    # if user does not want to play
     else:
+        print("Exiting program")
+        print(f"Final Scores: {scores_dict}")
         sys.exit()
 
 
-def initialise_deck():
-    """This function creates the deck and shuffles it."""
-    suit_template = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    four_suits = suit_template + suit_template + suit_template + suit_template
-    deck = (four_suits + four_suits + four_suits + four_suits)
-    print("Shuffling the deck...")
-    random.shuffle(deck)
-    #add if statement if games_played = n, shuffle deck
+def draw_card(hand, deck):
+    hand.append(deck.pop(0))
 
-    play_hands(deck)
-
-
-def play_hands(deck):
-    """Main play loop"""
-    player_hand = []
-    dealer_hand = []
-
-    player_hand.append(deck.pop(0))
-    dealer_hand.append(deck.pop(0))
-
-    player_hand.append(deck.pop(0))
-    dealer_hand.append(deck.pop(0))
-
-    print(f"Your Hand: {player_hand}")
-    print(f"Dealers Hand: {dealer_hand[1]}, x")
-    
-    play_player(deck, player_hand, dealer_hand)
+    if sum(hand) > 21 and 11 in hand:
+        replace = hand.index(11)
+        hand[replace] = 1
+        print("11 converted to 1")
 
 
 def play_player(deck, player_hand, dealer_hand):
 
+    if sum(player_hand) == 21:
+        print(f"Your Hand: {player_hand} - Blackjack!")
+        play_dealer(deck, player_hand, dealer_hand)
+
     player_inplay = True
-
-    print(f"Your hand: {player_hand} - {sum_of_list(player_hand)}")
-
     while player_inplay is True:
-        while sum(player_hand) > 21 and 11 in player_hand:
-                replace = player_hand.index(11)
-                player_hand[replace] = 1
 
         if sum(player_hand) > 21:
-                print("You lose!")
-                player_inplay = False
-                sys.exit()
+            player_inplay = False
+            outcome(player_hand)
         else:
+            print(f"Your Hand: {player_hand}")
+            print(f"Dealers Hand: {dealer_hand[1]}, x")
             draw_or_stick = input("Draw a card or stick? Draw/stick: ").lower()
             if draw_or_stick == "draw":
-                player_hand.append(deck.pop(0))
-                print(f"You draw a {player_hand[-1]}")
-                print(f"Your hand: {player_hand} - {sum_of_list(player_hand)}")
+                draw_card(player_hand, deck)
             elif draw_or_stick == "stick":
                 player_inplay = False
                 play_dealer(deck, player_hand, dealer_hand)
             else:
                 print("Invalid selection")
-                sys.exit()
-    
+
+
 def play_dealer(deck, player_hand, dealer_hand):
 
-    print(f"Dealers Hand: {dealer_hand} - {sum_of_list(dealer_hand)}")
+    print(f"Dealers Hand: {dealer_hand} - {sum(dealer_hand)}")
 
-    while sum_of_list(dealer_hand) < 17:
-        if sum_of_list(dealer_hand) >= 17:
-            dealer_inplay = False
-            winner()
-        else:
-            if sum_of_list(dealer_hand) >= sum_of_list(player_hand) and sum_of_list(dealer_hand) <= 21:
-                if sum_of_list(dealer_hand) == sum_of_list(player_hand):
-                    print(f"Your hand: {player_hand} - {sum_of_list(player_hand)} | {dealer_hand} - {sum_of_list(dealer_hand)}")
-                    print("It's a draw!")
-                else:
-                    print(f"Your hand: {player_hand} - {sum_of_list(player_hand)} | {dealer_hand} - {sum_of_list(dealer_hand)}")
-                    print("Dealer wins.")
-                    dealer_inplay = False
-                    sys.exit()
-            elif sum_of_list(dealer_hand) <= sum_of_list(player_hand) and sum_of_list(dealer_hand) < 17:
-                dealer_hand.append(deck.pop(0))
-                print(f"The dealer draws a {dealer_hand[-1]}")
-            else:
-                print(f"Your hand: {player_hand} - {sum_of_list(player_hand)} | {dealer_hand} - {sum_of_list(dealer_hand)}")
-                print("You win!")
-                sys.exit()
+    while sum(dealer_hand) < 17 or sum(dealer_hand) < sum(player_hand):
+        draw_card(dealer_hand, deck)
 
-def winner(player_hand, decker_hand):
+    outcome(player_hand, dealer_hand)
 
-### figure out dealer section and winner
-# 	if on 17, don’t draw anymore
-# 	get more than player
-	
-# 		if over 21 and has 11, change 11 to 1
-# 	bust
-	
-	
+
+def outcome(player_hand, dealer_hand=""):
+
+    print(
+        f"Your hand: {player_hand} - {sum(player_hand)} | {dealer_hand} - {sum(dealer_hand)}")
+
+    if sum(player_hand) > 21:
+        print("You bust!")
+        scores_dict['losses'] += 1
+    elif sum(dealer_hand) > 21:
+        print("Dealer busts! You win!")
+        scores_dict['wins'] += 1
+    elif sum(player_hand) > sum(dealer_hand):
+        print("You win!")
+        scores_dict['wins'] += 1
+    elif sum(player_hand) < sum(dealer_hand):
+        print("You lose!")
+        scores_dict['losses'] += 1
+    elif sum(player_hand) == sum(dealer_hand):
+        print("It's a draw!")
+        scores_dict['draws'] += 1
+
+    print(scores_dict)
+    start_game()
+
+
+scores_dict = {'played': 0, 'wins': 0, 'losses': 0, 'draws': 0}
 
 print(logo)
 start_game()
