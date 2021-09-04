6f2c5afec3b3d67e5f5f02a9c660fc8663ab5a5b
Benjamin Crew <benncrew94@gmail.com>
2021-09-04 17:15:01 +0100

    Day 29 - Complete

---
+++ 29/main.py
@@ -0,0 +1,110 @@
+from tkinter import *
+from tkinter import messagebox
+import pandas
+import random
+import pyperclip
+
+FONT="Courier"
+FONTSIZE = 14
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    total_num = 0
+
+    while total_num != 16:
+        nr_letters = random.randint(1,6)
+        nr_symbols = random.randint(1,6)
+        nr_numbers = random.randint(1,6)
+        total_num = nr_letters + nr_symbols + nr_numbers
+        password = ""
+
+    for n in range(nr_letters):
+        password += random.choice(letters)
+    for n in range(nr_symbols):
+        password += random.choice(symbols)
+    for n in range(nr_numbers):
+        password += random.choice(numbers)
+
+    password = list(password)
+    random.shuffle(password)
+    password = "".join(password)
+    pyperclip.copy(password)
+
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def clear_text():
+   website_entry.delete(0, END)
+   password_entry.delete(0, END)
+
+def add_password():
+    if any([len(a.get()) == 0 for a in (website_entry, password_entry, emailusername_entry)]):
+        messagebox.showerror(title="Missing Information!", message="Credentials not saved as not all entry boxes populated.")
+    else:
+        data = [
+            website_entry.get(),
+            emailusername_entry.get(),
+            password_entry.get()
+        ]
+
+        dataframe = pandas.DataFrame(columns=data)
+        dataframe.to_csv("creds.csv", mode='a')
+
+        clear_text()
+        messagebox.showinfo(title="Password saved.", message="Your password has been stored in the database.")
+# ---------------------------- CSV Creation ------------------------------- #
+try:
+    open("creds.csv")
+    # Do something with the file
+except IOError:
+    with open("creds.csv", mode="w") as file:
+
+        columns_list = [
+        "Website",
+        "Email/Username",
+        "Password"
+        ]
+
+        dataframe = pandas.DataFrame(columns=columns_list)
+        dataframe.to_csv("creds.csv", mode='w')
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Generator")
+window.config(bg="white", padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:", font=(FONT, FONTSIZE))
+emailusername_label = Label(text="Email/Username:", font=(FONT, FONTSIZE))
+password_label = Label(text="Password:", font=(FONT, FONTSIZE))
+
+website_label.grid(column=0, row=1)
+emailusername_label.grid(column=0, row=2)
+password_label.grid(column=0, row=3)
+
+website_entry = Entry(width=35)
+emailusername_entry = Entry(width=35)
+password_entry = Entry(width=21)
+
+website_entry.grid(column=1, row=1, columnspan=2)
+emailusername_entry.grid(column=1, row=2, columnspan=2)
+password_entry.grid(column=1, row=3)
+
+website_entry.focus()
+emailusername_entry.insert(0, "benncrew94@gmail.com")
+
+generate_button = Button(text="Generate Password", command=generate_password)
+add_button = Button(text="Add", width=36, command=add_password)
+
+generate_button.grid(column=2, row=3)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
