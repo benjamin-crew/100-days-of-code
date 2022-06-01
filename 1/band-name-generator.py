5201a2bed62f1c45b7d7684ec8d4ba1efd4df5d8
Benjamin Crew <benncrew94@gmail.com>
2022-06-01 15:09:16 +0100

    Updated day 1.

--- 1/band-name-generator.py
+++ 1/band-name-generator.py
@@ -1,7 +1,11 @@
-print("Welcome to the Band Name Generator.")
-town_city = input("What is the name of the town or city you grew up in?:\n")
-pet = input("And what is the name of your pet?:\n")
+def main():
+    print("Welcome to the Band Name Generator.")
+    town_city = input("What is the name of the town or city you grew up in?:\n")
+    pet = input("And what is the name of your pet?:\n")
 
-band_name = (town_city + " " + pet)
+    band_name = (town_city.capitalize() + " " + pet.capitalize())
 
-print("Your band name is: " + band_name + ".")
+    print("Your band name is: " + band_name + ".")
+
+if __name__ == "__main__":
+    main()
